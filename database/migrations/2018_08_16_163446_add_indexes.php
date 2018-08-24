97e69906566719323d4aa4746ee2288672720932
Pedro Maia <pedro@pedromm.com>
2018-08-24 17:09:48 -0300

    Fix migrations (#55)

--- database/migrations/2018_08_16_163446_add_indexes.php
+++ database/migrations/2018_08_16_163446_add_indexes.php
@@ -9,14 +9,17 @@ class AddIndexes extends Migration
     public function up()
     {
         Schema::table('proxies', function (Blueprint $table) {
-            $table->index(['anonymity_level', 'country', 'last_checked_at', 'last_status']);
+            $table->index('anonymity_level', 'anonymity_level_index');
+            $table->index('country', 'country_index');
+            $table->index('last_checked_at', 'last_checked_at_index');
+            $table->index('last_status', 'last_status_index');
         });
     }
 
     public function down()
     {
         Schema::table('proxies', function (Blueprint $table) {
-            $table->dropIndex(['anonymity_level', 'country', 'last_checked_at', 'last_status']);
+            $table->dropIndex(['anonymity_level_index', 'country_index', 'last_checked_at_index', 'last_status_index']);
         });
     }
 }
